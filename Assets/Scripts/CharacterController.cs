9d08b73bd64ef712569eb19f23437f7f693d7b58
Nick Dell'Aquilo <nickdellaquilo@protonmail.com>
2023-09-20 13:37:25 -0400

    Initial commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float maxSpeed = 5.0f;
+    public float jetpackStrength = 1.0f;
+    
+    public Camera mainCamera;
+
+    Rigidbody2D r2d;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
